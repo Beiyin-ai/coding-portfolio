7bf888f9a57cc12aae6802f6730ea5e0743bce58
Beiyin-ai <chiupeiyin08@gmail.com>
2025-12-16 13:52:47 +0000

    refactor: 專注Python解題，移除Kotlin檔案

--- algorithms/leetcode/easy/0009-palindrome-number/kotlin/Solution.kt
+++
@@ -1,9 +0,0 @@
-// 9. Palindrome Number
-# 解題日期: 13天前
-
-class Solution {
-    fun isPalindrome(x: Int): Boolean {
-        val str = x.toString()
-        return str == str.reversed()
-    }
-}
--- algorithms/leetcode/easy/2235-add-two-integers/kotlin/Solution.kt
+++
@@ -1,8 +0,0 @@
-// 2235. Add Two Integers
-// 解題日期: 13天前
-
-class Solution {
-    fun sum(num1: Int, num2: Int): Int {
-        return num1 + num2
-    }
-}
